e6705d2cadf10cbcd936be366b099f4a02bc71d6
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-12-19 20:48:31 +0100

    new file:   p3/docker-compose.yaml 	new file:   p3/p3.py 	renamed:    p3/productpages/Dockerfile -> p3/productpage/Dockerfile 	renamed:    p3/reviews/Dockerfile -> p3/reviewsv1/Dockerfile 	new file:   p3/reviewsv2/Dockerfile 	new file:   p3/reviewsv3/Dockerfile

---
+++ p3/p3.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+from subprocess import call
+import os
+
+
+os.system('sudo apt-get update')
+os.system('yes | sudo apt-get upgrade')
+os.system('sudo apt install docker-compose')
+os.system('sudo chmod +x /usr/local/bin/docker-compose')# + x--> permite ejecutar el archivo como programa
+
+
+os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
+os.chdir('practica_creativa2/bookinfo/src/reviews')
+os.system('sudo docker run --rm -u root -v "$(pwd)":/home/gradle/project -w /home/gradle/project gradle:4.8.1 gradle clean build')
+os.chdir(os.path.expanduser("~")) #??
+
+os.system('sudo docker-compose build')
+os.system('sudo docker-compose -f /home/hernangarqui/prueba2/bloque3/compose.yaml up')
