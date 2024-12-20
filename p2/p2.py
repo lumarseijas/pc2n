438ac96955e97e807a9a7cf5f20e4ab869040bee
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-12-20 13:50:52 +0100

    modified:   p2/p2.py

--- p2/p2.py
+++ p2/p2.py
@@ -3,25 +3,29 @@ import os
 import sys
 from subprocess import call
 
-call('sudo', 'apt', 'update')
-call('sudo', 'apt', 'install', 'docker.io')
+# Actualizar el sistema e instalar Docker
+#call(["sudo", "apt", "update"])
+#call(["sudo", "apt", "install", "-y", "docker.io"])
+
 def crear():
     # Crear la imagen de Docker
     print("[DEBUG] Construyendo la imagen de Docker...")
-    os.system("sudo docker build -t product-page/g24 . ")
+    os.system("sudo docker build -t product-page/24 . ")
     # Ejecutar el contenedor
     print("[DEBUG] Ejecutando el contenedor de Docker...")
-    os.system("sudo docker run --name product-page-g24 -p 5060:5060 -e GROUP_NUM=24 -d product-page/g24")  
+    os.system("sudo docker run --name product-page-g24 -p 5060:5060 -e GROUP_NUM=24 -d product-page/24")  
     print("[DEBUG] Contenedor ejecutándose en el puerto 5060.")
    
+
+from subprocess import call
 
 def liberar():
     print("[DEBUG] Liberando recursos...")
     # Detener y eliminar el contenedor
-    call("sudo","docker", "stop", "product-page-g24")
-    call("sudo", "docker", "rm", "product-page-g24")
-    call("sudo","docker", "rmi", "-f", "product-page/g24")
-    print("[INFO] No se encontró una imagen para eliminar.")
+    call(["sudo", "docker", "stop", "product-page-g24"])
+    call(["sudo", "docker", "rm", "product-page-g24"])
+    call(["sudo", "docker", "rmi", "-f", "product-page/24"])
+    print("[INFO] Contenedor y recursos eliminados correctamente.")
 
 comando = sys.argv[1]
 
