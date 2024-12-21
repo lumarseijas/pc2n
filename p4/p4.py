94f8409d4f944af33b92c8d2cff285b114f2b593
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-12-21 20:25:58 +0100

    new file:   p4/details.yaml 	modified:   p4/p4.py 	new file:   p4/productpage.yaml 	new file:   p4/ratings.yaml 	new file:   p4/reviews-svc.yaml 	new file:   p4/reviews-v1-deployment.yaml 	new file:   p4/reviews-v2-deployment.yaml 	new file:   p4/reviews-v3-deployment.yaml

---
+++ p4/p4.py
@@ -0,0 +1,18 @@
+#comando para Crear un cluster de Kubernetes en GKE con 3 nodos sin autoescalado
+#aplicar los archivos de configuracion: kubectl apply -f microservicio.yaml
+#verificar despliegues y servicios:
+# kubectl get pods
+# kubectl get services
+
+from subprocess import call
+#FALTA LO DE LOS 3 NODOS
+call("kubectl apply -f productpage.yaml", shell=True)
+call("kubectl apply -f details.yaml", shell=True)
+call("kubectl apply -f ratings.yaml", shell=True)
+call("kubectl apply -f reviews-svc.yaml", shell=True)
+call("kubectl apply -f reviews-v1-deployment.yaml", shell=True)
+call("kubectl apply -f reviews-v2-deployment.yaml", shell=True)
+call("kubectl apply -f reviews-v3-deployment.yaml", shell=True)
+
+call("kubectl get pods", shell=True)
+call("kubectl get services", shell=True)
