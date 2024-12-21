da46a77c36c496b9b49802cfabf6b9c4dd9d9ba1
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-12-21 20:36:34 +0100

    modified:   p4/p4.py

--- p4/p4.py
+++ p4/p4.py
@@ -6,22 +6,15 @@
 
 from subprocess import call
 #FALTA LO DE LOS 3 NODOS
-call(
-    "gcloud container clusters create pc2 "
-    "--num-nodes=3 "
-    "--enable-autoscaling=false "
-    "--region=us-central1 "
-    "--machine-type=e2-medium",
-    shell=True
-)
+call("minikube start --nodes 3",shell=True)
 
-call("kubectl apply -f productpage.yaml", shell=True)
-call("kubectl apply -f details.yaml", shell=True)
-call("kubectl apply -f ratings.yaml", shell=True)
-call("kubectl apply -f reviews-svc.yaml", shell=True)
-call("kubectl apply -f reviews-v1-deployment.yaml", shell=True)
-call("kubectl apply -f reviews-v2-deployment.yaml", shell=True)
-call("kubectl apply -f reviews-v3-deployment.yaml", shell=True)
+call("minikube kubectl apply -f productpage.yaml", shell=True)
+call("minikube kubectl apply -f details.yaml", shell=True)
+call("minikube kubectl apply -f ratings.yaml", shell=True)
+call("minikube kubectl apply -f reviews-svc.yaml", shell=True)
+call("minikube kubectl apply -f reviews-v1-deployment.yaml", shell=True)
+call("minikube kubectl apply -f reviews-v2-deployment.yaml", shell=True)
+call("minikube kubectl apply -f reviews-v3-deployment.yaml", shell=True)
 
-call("kubectl get pods", shell=True)
-call("kubectl get services", shell=True)
+call("minikube kubectl get pods", shell=True)
+call("minikube kubectl get services", shell=True)
