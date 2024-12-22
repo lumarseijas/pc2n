eedfe75adf970d3da76c8bff4ee8cbe4443b20a2
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-12-22 13:19:33 +0100

    modified:   p4/details.yaml 	modified:   p4/p4.py 	modified:   p4/productpage.yaml 	modified:   p4/ratings.yaml 	modified:   p4/reviews-v1-deployment.yaml 	modified:   p4/reviews-v2-deployment.yaml 	modified:   p4/reviews-v3-deployment.yaml

--- p4/p4.py
+++ p4/p4.py
@@ -6,15 +6,21 @@
 
 from subprocess import call
 
-call("minikube start --nodes 3",shell=True)
+#call("minikube start --nodes 3",shell=True)
+# Iniciamos un clúster de Kubernetes con 3 nodos
+call("minikube start --driver=docker -p pc2 --nodes 3", shell=True)
 
-call("minikube kubectl apply -f productpage.yaml", shell=True)
-call("minikube kubectl apply -f details.yaml", shell=True)
-call("minikube kubectl apply -f ratings.yaml", shell=True)
-call("minikube kubectl apply -f reviews-svc.yaml", shell=True)
-call("minikube kubectl apply -f reviews-v1-deployment.yaml", shell=True)
-call("minikube kubectl apply -f reviews-v2-deployment.yaml", shell=True)
-call("minikube kubectl apply -f reviews-v3-deployment.yaml", shell=True)
+# Cambiamos al perfil que acabamos de crear
+call("minikube profile pc2", shell=True)
+
+call("minikube kubectl apply -- -f productpage.yaml", shell=True)
+call("minikube kubectl apply -- -f details.yaml", shell=True)
+call("minikube kubectl apply -- -f ratings.yaml", shell=True)
+call("minikube kubectl apply -- -f reviews-svc.yaml", shell=True)
+call("minikube kubectl apply  -- -f reviews-v1-deployment.yaml", shell=True)
+call("minikube kubectl apply -- -f reviews-v2-deployment.yaml", shell=True)
+call("minikube kubectl apply -- -f reviews-v3-deployment.yaml", shell=True)
 
 call("minikube kubectl get pods", shell=True)
 call("minikube kubectl get services", shell=True)
+call("minikube service productpage", shell=True)
